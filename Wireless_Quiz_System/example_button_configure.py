7143ccfb2e898d8d3e918beb1656cf586996fb4b
Sounak Banerjee <sendtosounakbanerjee@gmail.com>
2026-08-08 01:54:43 +0530

    add example button configuration and SSD1306 OLED driver implementation

---
+++ Wireless_Quiz_System/example_button_configure.py
@@ -0,0 +1,22 @@
+#This is an Example code of how to configure the button IDs for the Wireless Quiz System.  You will need to edit this file to match the specific hardware configuration.
+# Master Team Dictionary (The 20-bit Remote IDs)
+# These are the calculated base IDs for all 8 of physical remotes
+teams = {
+    654891: "Team A",
+    14895672: "Team B",
+    89079842: "Team C",
+    54724157: "Team D",
+    579751: "Team E",
+    547788: "Team F",
+    59758921: "Team G",
+    891895: "Team H"
+}
+
+# Button ID Dictionary (The final 4 bits)
+# Mapped specifically to the output order (4, 8, 2, 1) from your test
+buttons = {
+    7: "Button A",
+    9: "Button B",
+    3: "Button C",
+    0: "Button D"
+}
